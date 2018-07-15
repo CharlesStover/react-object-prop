e72450234ff977f9bd840d226529a634960c5dc4
Charles Stover <github@charlesstover.com>
2018-07-15 17:39:47 -0500

    babel preset env

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,10 @@ module.exports = {
       {
         test: /\.js$/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: [ '@babel/preset-env' ]
+          }
         }
       }
     ]
